b97fe9f402eace843eaed0612e99ece8a76de8af
jocelynmedel <jocelynmedel@hotmail.com>
2019-11-27 18:08:22 -0600

    scala impl

--- activities/classwork/scala-intro/src/main/scala/example/a/Main.scala
+++ activities/classwork/scala-intro/src/main/scala/example/a/Main.scala
@@ -1,5 +1,16 @@
 package example.a
 
+import example.a.model.Timestamp
+
 object Main {
 
+  def main(args: Array[String]): Unit = {
+    val Array(s1,s2) = args
+
+    val t1 = Timestamp(seconds = s1.toInt)
+    val t2 = Timestamp(seconds = s2.toInt)
+
+    printLn(t1+t2)
+
+  }
 }
